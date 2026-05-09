40047791552dcd4d849e052cdbc8cbb7787d6011
MAubin <mazacra12345@gmail.com>
2026-05-09 13:35:35 -0400

    ajout humidite

--- app1/app1.ino
+++ app1/app1.ino
@@ -4,8 +4,58 @@
 #include "driver/gpio.h"
 
 #define LIGHT_PIN       GPIO_NUM_34
+#define HUMIDITY_PIN    GPIO_NUM_16
 
 Adafruit_DPS310 dps;
+
+float get_humidity() {
+  int i, j;
+  int duree[42];
+  unsigned long pulse;
+  byte data[5];
+  float humidite;
+  float temperature;
+  int broche = 16;
+
+  delay(2000);
+  
+  pinMode(broche, OUTPUT_OPEN_DRAIN);
+  digitalWrite(broche, HIGH);
+  delay(250);
+  digitalWrite(broche, LOW);
+  delay(20);
+  digitalWrite(broche, HIGH);
+  delayMicroseconds(40);
+  pinMode(broche, INPUT_PULLUP);
+  
+  while (digitalRead(broche) == HIGH);
+  i = 0;
+
+  do {
+        pulse = pulseIn(broche, HIGH);
+        duree[i] = pulse;
+        i++;
+  } while (pulse != 0);
+ 
+  if (i != 42) 
+    Serial.printf(" Erreur timing \n"); 
+
+  for (i=0; i<5; i++) {
+    data[i] = 0;
+    for (j = ((8*i)+1); j < ((8*i)+9); j++) {
+      data[i] = data[i] * 2;
+      if (duree[j] > 50) {
+        data[i] = data[i] + 1;
+      }
+    }
+  }
+
+  if ( (data[0] + data[1] + data[2] + data[3]) != data[4] ) 
+    Serial.println(" Erreur checksum");
+
+  return data[0] + (data[1] / 256.0);
+  Serial.printf(" Humidite = %4.0f \%%  Temperature = %4.2f degreC \n", humidite, temperature);
+}
 
 void init_light() {
   gpio_config_t io_conf = {};
@@ -46,12 +96,16 @@ void setup() {
 }
 
 void loop() {
-  float temperature, pressure;
+  float temperature, pressure, humidity;
   get_pressure_and_temp(temperature, pressure);
+  humidity = get_humidity();
 
   Serial.print("Temp: ");
   Serial.print(temperature);
   Serial.println(" °C");
+
+
+  Serial.printf("Humidite: %4.0f \%% \n", humidity);
 
   Serial.print("Pressure: ");
   Serial.print(pressure);
