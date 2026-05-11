f80e028ac8aec22b41c90b623638bde4e307978b
vince-smdt <vince.smdt@gmail.com>
2026-05-11 13:35:25 -0400

    Add functionnal light driver

---
+++ app1/light_driver.ino
@@ -0,0 +1,18 @@
+#ifndef LIGHT_DRIVER_H
+#define LIGHT_DRIVER_H
+
+#define LIGHT_PIN                   GPIO_NUM_34
+
+#define LIGHT_ADC_RESOLUTION        4095.0
+#define LIGHT_VOLTAGE_REF           3.3
+#define LIGHT_MAX_VOLTAGE           2.0
+#define R1_RESISTANCE               68_000.0  // in ohms
+
+// Returns brightness level as percentage (0-100)
+float get_light_perc() {
+  int raw = analogRead(LIGHT_PIN);
+  float voltage = LIGHT_VOLTAGE_REF * (raw / LIGHT_ADC_RESOLUTION);
+  return 100.0 * (voltage / LIGHT_MAX_VOLTAGE);
+}
+
+#endif // LIGHT_DRIVER_H
--- client/client.ino
+++ client/client.ino
@@ -1,11 +1,20 @@
 #include "driver/gpio.h"
 
+static int last_req_ts_ms = 0;
+static const int REQUEST_DELAY_MS = 5000;
+
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600, SERIAL_8N1, 21, 22);
+  last_req_ts_ms = millis();
 }
 
 void loop() {
-  Serial.println("This is a test");
-  delay(1000);
+  int now = millis();
+  if ((now - last_req_ts_ms) > REQUEST_DELAY_MS) {
+    Serial2.write('R');
+    last_req_ts_ms = now;
+  }
+
+  while (Serial2.available()) Serial.write(Serial2.read());
 }
